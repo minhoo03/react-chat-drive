1114434ee3d2f2029ab6c90694bccd6b76ec822b
minhoo03 <minhoo0333@gmail.com>
2021-02-21 17:54:40 +0900

    Message Header UI

--- src/components/ChatPage/MainPanel/MainPanel.js
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -5,17 +5,17 @@ import MeesageHeader from './MeesageHeader'
 
 export default function MainPanel() {
     return (
-        <div style={{paddding: '2rem 2rem 0 2rem'}}>
+        <div style={{width: '95%', margin:'0 auto', paddding: '2rem 2rem 0 2rem'}}>
             <MeesageHeader />
 
             <div style={{
                 width: '100%',
                 height: '50vh',
-                border: '.2rem solid #ececec',
+                border: '.1rem solid lightgray',
+                borderRadius:'4px',
                 padding: '1rem',
                 marginBottom: '1rem',
-                overflowY: 'auto',
-                backgroundColor:'lightgray'
+                overflowY: 'auto'
             }}>
 
             </div>
--- src/components/ChatPage/MainPanel/MeesageHeader.js
+++ src/components/ChatPage/MainPanel/MeesageHeader.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Container, Row, Col, InputGroup, FormControl, Image } from 'react-bootstrap'
+import { FaLock } from 'react-icons/fa'
+import { MdFavorite } from 'react-icons/md'
+import { BiSearchAlt2 } from 'react-icons/bi'
 
 function MeesageHeader() {
     return (
-        <div>
-            MeesageHeader
+        <div style={{
+            width: '100%',
+            height: '140px',
+            border: '.1rem solid lightgray',
+            borderRadius: '4px',
+            padding: '1rem',
+            marginTop: '1rem',
+            marginBottom: '1rem'
+        }}>
+            <Container>
+                <Row>
+                    <Col> <h4><FaLock /> ChatRoom <MdFavorite /></h4> </Col>
+                    <Col>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="basic-addon1"> <BiSearchAlt2 /></InputGroup.Text>
+                        </InputGroup.Prepend>
+
+                        <FormControl
+                            placeholder="Search a message..."
+                            aria-label="search"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col> description </Col>
+                    <Col> <Image src="" />{" "}user name </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
