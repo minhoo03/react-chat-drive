6b4800b37ee350a3536c27f48f7ece91c2300d24
minhoo03 <minhoo0333@gmail.com>
2021-02-21 18:12:46 +0900

    MessageForm UI

--- src/components/ChatPage/MainPanel/MeesageForm.js
+++ src/components/ChatPage/MainPanel/MeesageForm.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Col, Form, Row } from 'react-bootstrap'
 
 function MeesageForm() {
     return (
         <div>
-            MeesageForm
+            <Form>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Control as="textarea" rows={3} placeholder="Write a message." />
+                </Form.Group>
+            </Form>
+
+            <Row>
+                <Col>
+                    <button className="message-from-button" style={{width:'100%'}}> UPLOAD </button>
+                </Col>
+                <Col>
+                    <button className="message-from-button" style={{width:'100%'}}> SEND </button>
+                </Col>
+            </Row>
         </div>
     )
 }
